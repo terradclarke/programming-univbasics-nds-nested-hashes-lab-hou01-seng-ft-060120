81accbb080df6f450205b63f28319c2913fdfdde
Terranie Sims <terranie67@gmail.com>
2020-04-30 02:11:18 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,20 +6,47 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+def assembled_aoh 
+  result = [
+  {name:  "Don Gately", occupation: "Live-in Staff"},
+  { name:  "Joelle van Dyne", occupation: "Radio Personality" },
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  result = [
+  {:name =>  "Don Gately",:occupation => "Live-in Staff"},
+  {:name =>  "Joelle van Dyne",:occupation => "Radio Personality" },
+  {:name =>"Pat Monteseian",:occupation => "Staff" },
+  {:name =>  "Kate Gompert",:occupation => "None" },
+  {:name => "Bruce Green",:occupation => "Fan of Mildred" }
+  ]
 end
 
 def aoh_lookup(aoh, row, key)
+   result = [
+  {:name =>  "Don Gately",:occupation => "Live-in Staff"},
+  {:name =>  "Joelle van Dyne",:occupation => "Radio Personality" },
+  {:name =>"Pat Monteseian",:occupation => "Staff" },
+  {:name =>  "Kate Gompert",:occupation => "None" },
+  {:name => "Bruce Green",:occupation => "Fan of Mildred" }
+  ]
+  
+ result[2][:name]
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
+   result = [
+  {:name =>  "Don Gately",:occupation => "Live-in Staff"},
+  {:name =>  "Joelle van Dyne",:occupation => "Radio Personality" },
+  {:name =>"Pat Monteseian",:occupation => "Staff" },
+  {:name =>  "Kate Gompert",:occupation => "None" },
+  {:name => "Bruce Green",:occupation => "Fan of Mildred" }
+  ]
+  
+  result[1][:name] = "Joelle van Dyne (PGOAT)"
+  result
 end

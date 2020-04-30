8c92280eaf24128291bb9a015614a97c3a5f6258
Terranie Sims <terranie67@gmail.com>
2020-04-30 02:04:58 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,7 +17,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-  people_list = [
+  result = [
   {:name =>  "Don Gately",:occupation => "Live-in Staff"},
   {:name =>  "Joelle van Dyne",:occupation => "Radio Personality" },
   {:name =>"Pat Monteseian",:occupation => "Staff" },

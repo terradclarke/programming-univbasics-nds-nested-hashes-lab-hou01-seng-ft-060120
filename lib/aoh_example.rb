b3edbaf07aca1e91ad486cff99ae4785f0db27c9
Terranie Sims <terranie67@gmail.com>
2020-04-30 01:58:32 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -7,12 +7,12 @@ KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh 
-  [
+  people_list = [
   {name:  "Don Gately", occupation: "Live-in Staff"},
   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-  
-  
-  
+  { name:  "Pat Monteseian", occupation: "Staff" },
+  { name:  "Kate Gompert", occupation: "None" },
+  { name:  "Bruce Green", occupation: "Fan of Mildred" }
   ]
 end
 

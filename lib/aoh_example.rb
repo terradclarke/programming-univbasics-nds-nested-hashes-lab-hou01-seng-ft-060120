4f75e603f50ac07ac4269a21d9aa23bb0e6f46a2
Terranie Sims <terranie67@gmail.com>
2020-04-30 02:06:09 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -27,6 +27,14 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+   result = [
+  {:name =>  "Don Gately",:occupation => "Live-in Staff"},
+  {:name =>  "Joelle van Dyne",:occupation => "Radio Personality" },
+  {:name =>"Pat Monteseian",:occupation => "Staff" },
+  {:name =>  "Kate Gompert",:occupation => "None" },
+  {:name => "Bruce Green",:occupation => "Fan of Mildred" }
+  ]
+  
  result[0][:name]
 end
 
